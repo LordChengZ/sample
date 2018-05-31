ad36b104420f93a59c6f9fcc7aa37174730b9680
LordCzH <673212712@qq.com>
2018-05-31 06:36:54 +0000

    Finish login and logout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -11,9 +11,9 @@
     	<div class="col-md-offset-1 col-md-10">
         @include('shared._messages')
     		@yield('content')
-    		@include('layouts._footer')
     	</div>
-      
+        @include('layouts._footer')
     </div>    
+    <script type="text/javascript" src="/js/app.js"></script>
   </body>
 </html>
